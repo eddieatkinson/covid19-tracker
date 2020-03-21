24d95d5ca3dd2641b5f5984b046df8c94b0c286a
Eddie Atkinson <eddiebatkinson@gmail.com>
2020-03-21 12:45:11 -0400

    state dropdown with info retrieving

--- src/App.js
+++ src/App.js
@@ -1,34 +1,65 @@
 import React, { useState, useEffect } from "react";
-import { fetchData } from "./utilities";
+import { states } from "./utilities";
+import { map, findLast } from "lodash";
 
 function App() {
-  const [data, setData] = useState(["people", "temple"]);
-  const [useLocation, setUseLocation] = useState(false);
+  const [data, setData] = useState([]);
+  // const [useLocation, setUseLocation] = useState(false);
   const [userLocation, setUserLocation] = useState();
+  const [usState, setUsState] = useState();
+  const [stateData, setStateData] = useState();
 
   useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(position =>
-        console.log(position)
-      );
-      fetchData("us", "confirmed");
-    } else {
-      alert("Geolocation is not supported by this browser.");
-    }
-  });
+    console.log("running");
+    // navigator.geolocation.getCurrentPosition(position =>
+    //   console.log(position)
+    // );
+    const country = "us";
+    const status = "confirmed";
+    fetch(
+      `https://api.covid19api.com/dayone/country/${country}/status/${status}`
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(dataReturned => {
+        setData(dataReturned);
+        console.log(dataReturned);
+      });
+  }, []);
 
   const updateInput = e => {
-    setUseLocation(e.target.checked);
+    // setUseLocation(e.target.checked);
   };
+  console.log(data);
+  function handleInput(e) {
+    setUsState(e.target.value);
+    const latestInfo = findLast(data, datum => {
+      return datum.Province === e.target.value;
+    });
+    const stateInfo = latestInfo
+      ? latestInfo.Cases
+      : "Information not available";
+    setStateData(stateInfo);
+  }
 
   return (
     <div>
       <form>
-        <label>
-          Use current location:
-          <input name="useLocation" type="checkbox" onChange={updateInput} />
-        </label>
+        <label>Select a state:</label>
+        <select onChange={handleInput} id="states">
+          <option>Select a state...</option>
+          {map(states, (state, i) => {
+            return (
+              <option key={i} value={state}>
+                {state}
+              </option>
+            );
+          })}
+        </select>
       </form>
+      <p>{usState}</p>
+      <p>{stateData}</p>
     </div>
   );
 }
